3141d054585563c9857056b0e595e930c8995016
Peter Noah <peternoah295@gmail.com>
2022-07-08 08:32:03 -0700

    Updates

--- js/pages/form-wizard.js
+++ js/pages/form-wizard.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
             var $total = navigation.find('li').length;
             var $current = index+1;
             var $percent = ($current/$total) * 100;
-            $('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
+            $('#rootwizard').find('.bg-success').css({width:$percent+'%'});
         },
         'onNext': function(tab, navigation, index) {
             var $valid = $("#wizardForm").valid();
@@ -27,6 +27,23 @@ $(document).ready(function() {
             }
             document.getElementById('prev').style.display = 'block';
             document.getElementById('next').style.display = 'none';
+
+            var wide = 100;
+            var identity = setInterval(scene, 1000);
+            function scene() {
+                if(wide <= 50){
+                    clearInterval(identity);
+                } else {
+                    wide--;
+                    var minutes = Math.floor(wide/60);
+                    var seconds = wide - minutes * 60;
+                    if(seconds < 10){
+                        seconds = '0'+seconds
+                    }
+                    $('#rootwizard').find('.bg-success').css({width:wide+'%'});
+                    document.getElementById('escoz').innerText = `Time left: ${minutes}:${seconds}`;
+                }
+            }
         },
         'onPrevious': function(tab, navigation, index) {
             document.getElementById('prev').style.display = 'none';
