71bc6142a361d5beb04c85c72d7ce1e682a2435b
Peter Noah <peternoah295@gmail.com>
2022-07-09 21:23:33 +0300

    ch

--- public/js/main/maincoin.js
+++ public/js/main/maincoin.js
@@ -35,6 +35,20 @@
 
 
     $(document).ready(function() {
+        if ($(".countdown").length !== 0) {
+            var tpj = jQuery;
+            var countdown;
+            tpj(document).ready(function() {
+                if (tpj(".countdown").countdown == undefined) {
+                    revslider_showDoubleJqueryError(".countdown");
+                } else {
+                    countdown = tpj(".countdown").show().countdown({
+                        date: "7 Aug 2022 00:01:00",
+                        format: "on"
+                    });
+                }
+            });
+        }
         $('#services-carousel').owlCarousel({
             loop: true,
             responsiveClass: true,
