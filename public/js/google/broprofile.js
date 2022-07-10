5a1ca0c32b147cfafa7b7626e83d28d8d65181bc
Peter Noah <peternoah295@gmail.com>
2022-07-10 23:20:01 +0300

    ch

--- public/js/google/broprofile.js
+++ public/js/google/broprofile.js
@@ -29,7 +29,7 @@ const auth = firebase.auth();
 auth.onAuthStateChanged(user => {
 	let goodies = [];
 	if (!user) {
-		// window.location.assign("index");
+		window.location.assign("index");
 	}
 	if (user.photoURL) {
 		avatarHolder.setAttribute("src", user.photoURL);
