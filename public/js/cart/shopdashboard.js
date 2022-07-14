3ac3d178d6b447d77f0648918672f3b20ec305ba
Peter <peternoah295@gmail.com>
2022-07-14 15:49:08 +0300

    ch

--- public/js/cart/shopdashboard.js
+++ public/js/cart/shopdashboard.js
@@ -57,38 +57,6 @@ if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklo
         button.addEventListener('click', removeCartItem)
     }
     updateCartTotal();
-
-
-    for(var i = 0; i < items.length; i++) {
-        var cartRow = document.createElement('tr');
-        var cartRow2 = document.createElement('li');
-        cartRow.classList.add('table-warning');
-        cartRow2.classList.add('total','bg-black');
-        var cartItems =  document.getElementsByClassName('champez')[0];
-        var cartRowContents = `
-            <td>
-                <span class="label label-warning">Pending<i class="fas fa-spin fa-sync-alt spinner-bordez"></i></span>
-            </td>
-            <td class="btn-balance">${(items[i].balance).replace('Balance: ','')}</td>
-            <td><img src=${items[i].image}></td>
-            <td id=${'name-on-table' + items.indexOf(items[i])} style="filter: blur(0px);"></td> 
-            <td>${items[i].account}</td>
-            <td class="btn-price">${(items[i].price).replace('Price: ','')}</td>
-            <td>${items[i].info1}</td>
-            <td>${items[i].info2}</td>
-            <td>${items[i].info3}</td>
-            <td>${items[i].info4}</td>
-            <td>${items[i].info5}</td>
-            <td>${items[i].info6}</td>
-            <td>${items[i].info7}</td>
-            <td>${items[i].website}</td>
-        `;
-
-        cartRow.innerHTML = cartRowContents;
-
-        cartItems.prepend(cartRow);
-        updateCartTotal();
-    }
 } else {
     document.getElementById('cartlength').style.display = 'none';
 }
