09e0ed0cef2e393616bb48d195174f77dea86c45
Peter <peternoah295@gmail.com>
2022-07-14 21:30:50 +0300

    ch

--- public/js/google/broinvoice.js
+++ public/js/google/broinvoice.js
@@ -46,15 +46,7 @@ auth.onAuthStateChanged(user => {
 		jinaHolder.innerText = user.phoneNumber;
 		jinaHolder2.innerText = 'User ID: ' + user.uid;
 		invoiceHolder.innerHTML = "Invoice to: " + user.phoneNumber;
-	} else if(user.isAnonymous && user.displayName){
-		jinaHolder.innerText = user.displayName;
-		jinaHolder2.innerText = 'User ID: ' + user.uid;
-		invoiceHolder.innerText = 'User ID: ' + user.uid;
-	} else if(user.isAnonymous && !user.displayName){
-		jinaHolder.innerText = 'Anonymous';
-		jinaHolder2.innerText = 'User ID: ' + user.uid;
-		invoiceHolder.innerText = 'User ID: ' + user.uid;
-	}
+	} 
 });
 
 
